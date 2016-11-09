661cd163be05110d16d2e86f8b95b31ccb9f7042
Chi Vinh Le <vinh@wikiwi.io>
2016-11-09 22:37:34 +0700

    Overhaul build, test and lint process

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const tslint = require("gulp-tslint");
@@ -5,57 +7,46 @@ const jsonlint = require("gulp-jsonlint");
 const eslint = require("gulp-eslint");
 const merge = require("merge2");
 const yamllint = require("gulp-yaml-validate");
-const ignore = require("gulp-ignore");
+const sourcemaps = require("gulp-sourcemaps");
 
-const testFiles = ["*.spec.tsx", "*.spec.ts"];
+const files = {
+  tsWithoutTest: ["./src/**/*.ts", "./src/**/*.tsx", "!./src/**/*.spec.tsx", "!./src/**/*.spec.ts"],
+  tsWithTest: ["./src/**/*.ts", "./src/**/*.tsx", "test/**/*.ts", "test/**/*.tsx"],
+  json: ["./*.json", "./.nycrc"],
+  yaml: ["./*.yml"],
+  js: ["./*.js"],
+};
 
-gulp.task("es6", ["lint"], () => {
-  const tsProject = ts.createProject("tsconfig.json", {
-    declaration: true,
-    target: "es6",
-    module: "es6",
-  });
-  const tsResult = tsProject.src()
-    .pipe(ignore.exclude(testFiles))
-    .pipe(tsProject());
-  return merge([
-    tsResult.dts.pipe(gulp.dest("dist/typings")),
-    tsResult.js.pipe(gulp.dest("dist/es6")),
-  ]);
-});
+function onBuildError() {
+  this.once("finish", () => process.exit(1));
+}
 
-gulp.task("esm", ["lint"], () => {
-  const tsProject = ts.createProject("tsconfig.json", {
-    declaration: true,
-    target: "es5",
-    module: "es6",
-  });
-  const tsResult = tsProject.src()
-    .pipe(ignore.exclude(testFiles))
-    .pipe(tsProject());
-  return merge([
-    tsResult.dts.pipe(gulp.dest("dist/typings")),
-    tsResult.js.pipe(gulp.dest("dist/esm")),
-  ]);
-});
+function build(dest, module) {
+  return () => {
+    const tsProject = ts.createProject("tsconfig.json", {
+      noEmitOnError: true,
+      declaration: dest === "lib",
+      target: "es5",
+      module,
+    });
+    const tsResult = gulp.src(files.tsWithoutTest)
+      .pipe(sourcemaps.init())
+      .pipe(tsProject())
+      .once("error", onBuildError);
+    return merge([
+      tsResult.dts.pipe(gulp.dest(dest)),
+      tsResult.js
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(dest)),
+    ]);
+  };
+}
 
-gulp.task("commonjs", ["lint"], () => {
-  const tsProject = ts.createProject("tsconfig.json", {
-    declaration: true,
-    target: "es5",
-    module: "commonjs",
-  });
-  const tsResult = tsProject.src()
-    .pipe(ignore.exclude(testFiles))
-    .pipe(tsProject());
-  return merge([
-    tsResult.dts.pipe(gulp.dest("dist/typings")),
-    tsResult.js.pipe(gulp.dest("dist/commonjs")),
-  ]);
-});
+gulp.task("lib", build("lib", "es6"));
+gulp.task("commonjs", build("cjs", "commonjs"));
 
 gulp.task("tslint", () => {
-  return gulp.src("./src/**/*.ts")
+  return gulp.src(files.tsWithTest)
     .pipe(tslint({
       formatter: "verbose",
     }))
@@ -63,26 +54,22 @@ gulp.task("tslint", () => {
 });
 
 gulp.task("eslint", () => {
-  return gulp.src("./*.js", { dot: true })
+  return gulp.src(files.js, { dot: true })
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
 gulp.task("jsonlint", () => {
-  return gulp.src([
-    "./*.json",
-    "./test/**/*.json",
-    "./src/**/*.json",
-  ], { dot: true })
+  return gulp.src(files.json, { dot: true })
     .pipe(jsonlint())
     .pipe(jsonlint.reporter());
 });
 
 gulp.task("yamllint", () => {
-  return gulp.src("./*.yml", { dot: true })
+  return gulp.src(files.yaml, { dot: true })
     .pipe(yamllint());
 });
 
 gulp.task("lint", ["jsonlint", "eslint", "tslint", "yamllint"]);
-gulp.task("default", ["lint", "commonjs", "esm", "es6"]);
+gulp.task("default", ["lib", "commonjs"]);
